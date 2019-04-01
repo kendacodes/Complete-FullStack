411950d0952e06b8aae72d8f86c055b1389fd3b1
KendaCodes <K.Lamandier@gmail.com>
2019-03-31 21:38:48 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://yonce:yonce@yonce-6abmg.mongodb.net/Hive?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'Hive'
+};
